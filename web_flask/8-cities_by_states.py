2bdd0cd25f9e5c14d56c33e95507863c891e36cb
McLarrm <larrm_ola@live.com>
2023-11-05 04:44:05 -0800

    Script that starts a Flask web application

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,18 +11,19 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities():
-    """Returns a rendered html template
+def cities_by_states():
+    """Returns a rendered HTML template
     at the /cities_by_states route,
     listing the cities by states"""
-    return render_template('8-cities_by_states.html',
-                           states=storage.all('State').values())
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
     """Removes the current SQLAlchemy Session"""
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
