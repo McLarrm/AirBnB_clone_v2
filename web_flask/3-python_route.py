f7b1ea2412e2ae04bd1f7fc8dad98c88c9d30fad
McLarrm <larrm_ola@live.com>
2023-11-05 04:11:19 -0800

    Script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,5 +24,12 @@ def hello_text(text):
     """ Third Route that display C and text"""
     return "C {}".format(text.replace("_", " "))
 
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text):
+    """ Fourth Route that display C and text"""
+    return "Python {}".format(text.replace("_", " "))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
